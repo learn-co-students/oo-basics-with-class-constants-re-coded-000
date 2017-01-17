cab1c7b3dc7c926ea9045d4167a6cc2677dd99a8
Muataz Aziz <muataz.aziz91@yahoo.com>
2017-01-17 18:42:48 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+BRANDS= []
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+    # BRANDS << brand : BRANDS.include?(brand)
+    # BRANDS << brand : BRANDS != BRANDS.uniq
   end
 
   def cobble
